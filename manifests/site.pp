dbc48f44c0ea8386459ef432303262258172a958
HA User <ha_user@example.com>
2016-11-17 14:30:46 -0800

    add /ha_test/control_repo/manifests/site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,9 @@
-node default { notify { 'gamma': } }
+site {
+  app1 { 'app1name':
+    nodes => {
+      Node['x063ji4rzav0ja8.delivery.puppetlabs.net'] => [App1::Comp1['foo1'], ],
+      Node['v8bdpz6da23uzyk.delivery.puppetlabs.net'] => [App1::Comp2['foo2'], ],
+    },
+  }
+}
+
