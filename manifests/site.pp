6a32323d32aff3141327e61a932cc23616d1206e
HA User <ha_user@example.com>
2016-11-17 17:31:50 -0800

    add /ha_test/control_repo/manifests/site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,8 @@
 site {
   app1 { 'app1name':
     nodes => {
-      Node['i6e51u0py4aivkq.delivery.puppetlabs.net'] => [App1::Comp1['foo1'], ],
-      Node['nviwzw47i0f5jvp.delivery.puppetlabs.net'] => [App1::Comp2['foo2'], ],
+      Node['kjtkvq9h5m8fgc1.delivery.puppetlabs.net'] => [App1::Comp1['foo1'], ],
+      Node['j7k8r897rw1hljp.delivery.puppetlabs.net'] => [App1::Comp2['foo2'], ],
     },
   }
 }
