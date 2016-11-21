6f932fc5c25062f920b3a76101de162a5594be0c
HA User <ha_user@example.com>
2016-11-21 10:18:19 -0800

    add new content

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,4 @@
-site {
-  app1 { 'app1name':
-    nodes => {
-      Node['gym9luhuwfuqaa8.delivery.puppetlabs.net'] => [App1::Comp1['foo1'], ],
-      Node['rcqt3jgqzwgvjhv.delivery.puppetlabs.net'] => [App1::Comp2['foo2'], ],
-    },
-  }
+node default {
+  notify { 'foo': }
 }
 
