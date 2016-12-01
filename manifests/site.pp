d9aad5cee81a8a6fe6a560375cd35046dc1cca81
HA User <ha_user@example.com>
2016-12-01 10:14:18 -0800

    add /ha_test/control_repo/manifests/site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,8 @@
 site {
   app1 { 'app1name':
     nodes => {
-      Node['rf6ztyhf3vvd90z.delivery.puppetlabs.net'] => [App1::Comp1['foo1'], ],
-      Node['o2zayk77bux3640.delivery.puppetlabs.net'] => [App1::Comp2['foo2'], ],
+      Node['fnewkdwkyu4l0ef.delivery.puppetlabs.net'] => [App1::Comp1['foo1'], ],
+      Node['xwzpglns0pnvgpl.delivery.puppetlabs.net'] => [App1::Comp2['foo2'], ],
     },
   }
 }
